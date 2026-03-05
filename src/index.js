f76ae3cab99f7d583d2641f2d1017877b350ef29
Sushant Vangapandu <vsushant97@gmail.com>
2026-03-05 16:11:14 +0530

    fix worker code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 export default {
     async fetch(request) {
+
         try {
-            return await fetch("https://afullstackguy.dev", {
-                cf: { timeout: 3 }
-            })
-        } catch (e) {
-            return fetch("https://backup.afullstackguy.dev")
+            const res = await fetch(request, { cf: { timeout: 2 } });
+
+            if (res.ok) {
+                return res;
+            }
+
+            throw new Error("primary failed");
+
+        } catch {
+
+            const url = new URL(request.url);
+
+            const backupUrl = "https://backup.afullstackguy.dev" + url.pathname + url.search;
+
+            return fetch(backupUrl);
         }
     }
 }
